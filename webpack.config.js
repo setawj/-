4ecf3a36f08bcd282f05309fbe90c9b483ccce56
setawj <setajs@naver.com>
2018-04-13 03:59:05 +0900

    webpack 3.10

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
 
-const config = {
-    entry : './src/app.js',
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractCSS = new ExtractTextPlugin('[name].bundle.css')
+
+module.exports = {
+    entry : {
+        app : ['./src/app.js']
+    },
     output : {
-        filename : 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename : '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ["es2015"] }
+                }
+            }
+        ]
+    },
+    plugins: [
+        extractCSS
+    ],
+    resolve: {}
 };
-
-module.exports = config;
